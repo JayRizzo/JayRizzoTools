69b43732e6f01777042234104bb1f417103c52c9
Jeromie Kirchoff <jahomie04@gmail.com>
2022-07-12 15:57:03 -0500

    Get a random poker hand served.

---
+++ pyGenHand.py
@@ -0,0 +1,43 @@
+from random import choice
+from random import shuffle
+from random import randrange
+from itertools import product
+from collections import defaultdict
+from copy import deepcopy # https://stackoverflow.com/a/2465951/1896134
+
+# print(u"\u2666") # diamond ♦
+# print(u"\u2665") # heart ♥
+# print(u"\u2663") # club ♣
+# print(u"\u2660") # spade ♠
+
+class Poker(object):
+    def __init__(self):
+        self.Suit = [u"\u2666", u"\u2665", u"\u2663", u"\u2660"]
+        self.SuitLen = len(self.Suit)
+        self.Card = ['A','K','Q','J','10','9','8','7','6','5','4','3','2']
+        self.Card = [x.rjust(2) for x in self.Card]
+        self.CardLen = len(self.Card)
+        self.Suit = [u"\u2666", u"\u2665", u"\u2663", u"\u2660"]
+        self.FullDeck = defaultdict()
+        self.FullDeck = list(product(tuple(self.Card), tuple(self.Suit)))
+        self.FullDeck = dict((i,j) for i,j in enumerate(self.FullDeck))
+
+    def dealHand(self, NumberOfCards):
+        self.HandDealt = []
+        self.UpdatedDeck = deepcopy(self.FullDeck)
+        try:
+            for i in range(NumberOfCards):
+                self.Key, self.Value = choice(list(self.UpdatedDeck.items()))
+                if self.Value not in self.HandDealt:
+                    self.HandDealt.append(self.Value)
+                    del self.UpdatedDeck[self.Key]
+        except IndexError as e:
+            print('Ran out of cards to deal You have the Whole Deck Listed.')
+        print(f"Hand Dealt: {self.HandDealt}")
+
+if __name__ == '__main__':
+    a = Poker()
+    a.dealHand(2)
+    a.dealHand(3)
+    a.dealHand(4)
+    a.dealHand(5)
