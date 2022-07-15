d10ce7f0a55dec1a1fca85390b3b48011540bf59
Jeromie Kirchoff <jahomie04@gmail.com>
2022-07-15 18:15:34 -0500

    removed Emoji Generation as it was misbehaving from what i expected.

--- pyGenRandHEX.py
+++ pyGenRandHEX.py
@@ -4,8 +4,8 @@
 # Created Syst: macOS Monterey 12.4 (21F79) Kernel: Darwin 21.5.0
 # Created Plat: Python 3.10.5 ('v3.10.5:f377153967', 'Jun  6 2022 12:36:10')
 # Created By  : Jeromie Kirchoff
-# Created Date: Mon Jun 11 12:46:00 2022 PDT
-# Last ModDate: Tue Jul 12 16:48:57 2022 CDT
+# Created Date: Mon Jun 11 14:46:00 2022 CDT
+# Last ModDate: Fri Jul 15 18:12:20 2022 CDT
 # =============================================================================
 # Notes:
 # =============================================================================
@@ -16,21 +16,11 @@ from string import hexdigits
 def generateRandomHEX():
     randomHEX = [0]*6  # A list variable to hold empty list.
     i = 0  # Declare and set loop counter to 0.
-    for i in range (0,6):
+    for i in range(0,6):
         randomHEX[i] = choice(hexdigits[0:16])
     str1 = ''.join(str(e) for e in randomHEX)
-    return f"{str1}"
-
-def generateEmojiHEX():
-    randomHEX = [0]*8  # A list variable to hold empty list.
-    i = 0  # Declare and set loop counter to 0.
-    for i in range (0,8):
-        randomHEX[i] = choice(hexdigits[0:16])
-    str1 = ''.join(str(e) for e in randomHEX)
-    str1 = f"\'\\U{str1}\'"
     return f"{str1}"
 
 if __name__ == '__main__':
     print(f"Your Random HEX Color is: {generateRandomHEX()}")
-    print(f"Your Random HEX Emoji is: {generateEmojiHEX()}")
 
